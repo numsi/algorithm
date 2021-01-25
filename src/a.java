55d9fcc68a8705b08980bdad099f433883317661
numsi <wangqppp@gmail.com>
2021-01-25 17:29:33 +0800

    测试上传

---
+++ src/a.java
@@ -0,0 +1,13 @@
+/**
+ * @author numsi
+ * @date 2020/10/25 22:21
+ */
+import edu.princeton.cs.algs4.*;
+public class a {
+    public static void main(String[] args) {
+        System.out.println("这是体格测试");
+        StdOut.print("a");
+    }
+
+
+}
